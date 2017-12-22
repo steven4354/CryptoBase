937d84adca9a0aff0ac8834bb08c674c7d95f71a
Steven Li <steven4354@gmail.com>
2017-12-22 15:47:06 -0800

    created the dashboard

--- routers/home.js
+++ routers/home.js
@@ -10,6 +10,10 @@ const mongoose = require("mongoose");
 const {User} = require("./../models");
 const passport = require("passport");
 
+//for making requests to apis
+const request = require("request");
+const fetch = require("node-fetch");
+
 // 1
 router.get("/", (req, res) => {
   if (req.user) {
@@ -62,12 +66,119 @@ router.get("/logout", function(req, res) {
   res.redirect("/");
 });
 
-//Dashboard Home
-router.get("/home", function(req, res) {
-  if (req.user) {
-    res.render("home");
-  } else {
-    res.redirect("/login");
+// ----------------------------------------
+// DashBoard Home
+// ----------------------------------------
+
+//main view
+router.get("/home", async (req, res, next) => {
+  try {
+    if (req.user) {
+      //getting the user
+      const email = req.user.email;
+      const user = await User.findOne({email}, function(err, user) {
+        return user;
+      });
+
+      //grabbing the list of coins
+      const coinsList = await fetch("https://api.coinmarketcap.com/v1/ticker/")
+        .then(function(res) {
+          return res.json();
+        })
+        .then(function(json) {
+          return json;
+        });
+
+      //grabbing the coins of the user
+      const userCoins = coinsList.filter(coinObj => {
+        return user.coinsSym.includes(coinObj.symbol);
+      });
+
+      //setting up the url for the news
+      let coinNamesForURL = userCoins.map(obj => obj.name.toLowerCase());
+      if (coinNamesForURL.length === 0) {
+        coinNamesForURL = ["bitcoin"];
+      } else {
+        coinNamesForURL = coinNamesForURL.splice(0, 2);
+      }
+
+      let articles = await fetch(
+        `https://newsapi.org/v2/everything?q=${coinNamesForURL.join(
+          ","
+        )}&domains=google.com,yahoo.com,wsj.com,nytimes.com&from=2017-12-01&to=2017-12-21&apiKey=76cba483de7544e48b095c1965cca39f`
+      )
+        .then(function(res) {
+          return res.json();
+        })
+        .then(function(json) {
+          return json.articles;
+        });
+
+      res.render("home", {
+        articles: articles,
+        coinsList: coinsList,
+        userCoins: userCoins
+      });
+    } else {
+      res.redirect("/login");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//adding a new coin
+router.post("/home/addcoin", async (req, res, next) => {
+  try {
+    //finding the user
+    const email = req.user.email;
+    const user = await User.findOne({email}, function(err, user) {
+      return user;
+    });
+
+    //grabbing the list of coins
+    const coinsList = await fetch("https://api.coinmarketcap.com/v1/ticker/")
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(json) {
+        return json;
+      });
+
+    //making sure the coin hasn't been added and then adding it note: req.body.coin is a symbol like BTC
+    if (!user.coinsSym.includes(req.body.coin)) {
+      await user.coinsSym.push(req.body.coin);
+      await user.save();
+      res.redirect("/");
+    } else {
+      res.redirect("/");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//removing a coin
+router.post("/home/removecoin", async (req, res, next) => {
+  try {
+    //finding the user
+    const email = req.user.email;
+    const user = await User.findOne({email}, function(err, user) {
+      return user;
+    });
+
+    //making sure the coin hasn't been added and then adding it note: req.body.coin is a symbol like BTC
+    user.coinsSym = await user.coinsSym.filter(string => {
+      if (string === req.user.coin) {
+        return false;
+      }
+      return true;
+    });
+
+    await user.save;
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
   }
 });
 
